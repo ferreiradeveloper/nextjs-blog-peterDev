c42f7482c65df6d860b66c0ad44a1c0265128aa9
Francisco Ferreira de la cruz <ferreiradelacruz@hotmail.com>
2021-08-02 18:58:04 +0200

    primer commit

---
+++ pages/contact.jsx
@@ -0,0 +1,12 @@
+import Layout from "../components/Layout";
+
+export default function about() {
+    return (
+        <Layout
+        title="Contac | Next.js"
+        description= "como me contactas"
+        >
+            <h1>Contac</h1>
+        </Layout>
+    )
+}
